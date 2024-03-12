32378c5f6828bb03ec9540b5d15dc4e43e606015
s25917 <s25917@pjwstk.edu.pl>
2024-03-12 17:00:16 +0100

    Add GetAverage method

--- ninePlusTen21/ninePlusTen21/Program.cs
+++ ninePlusTen21/ninePlusTen21/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum = sum + numbers[i];
+    }
+    return sum / numbers.Length;
+}
